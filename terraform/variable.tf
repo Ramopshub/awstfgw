7f8d6b80cef6826fb3d8ce0070b3cd38970ac599
Ramopshub <96540687+Ramopshub@users.noreply.github.com>
2025-02-08 19:33:29 +0530

    Create variable.tf

---
+++ terraform/variable.tf
@@ -0,0 +1,10 @@
+variable "ami_id" {
+  description = "The AMI ID to use for the EC2 instance."
+  type        = string
+}
+
+variable "instance_type" {
+  description = "The instance type for the EC2 instance."
+  type        = string
+  default     = "t2.micro"  # Default instance type if none is provided
+}
